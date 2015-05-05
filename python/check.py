98f8551f405dc03fe59ac42b96cee59e090d8da8
Oliver Mueller <oliver.mueller@esrlabs.com>
2015-05-05 10:37:14 +0200

    working python solution (single threaded)

--- python/check.py
+++ python/check.py
@@ -26,10 +26,10 @@ def ping(url):
         return [e.code, False, None]
     except urllib2.URLError, e:
         logging.error(url + ' resulted in URLError = ' + str(e.reason))
-        return [0, False, None]
+        return [str(e.reason), False, None]
     except httplib.HTTPException, e:
         logging.error(url + ' resulted in HTTPException')
-        return [0, False, None]
+        return [e, False, None]
     except Exception:
         logging.error(url + ' resulted in Exception')
         return [0, False, None]
@@ -51,22 +51,24 @@ def checkDomain(url, domain):
 
 def checkUrlRecursiv(starturl, domain):
     links2check = {starturl : ''}
-    checked = set()
+    checked = {}
     while len(links2check) > 0:
         (url,origin) = links2check.popitem()
-        logging.info(">>>>>>>>> checking: %s (%d links to go)", url, len(links2check))
+        logging.info("[%d links to go] >>>>>>>>> checking: %s", len(links2check), url)
         [status,isText,realurl] = ping(url)
-        checked.add(url)
+        checked[url] = (status,origin)
         if (status >= 200 and status < 300):
             logging.info("%s is responding (origin:%s)[status %d]", url, origin, status)
             if checkDomain(url, domain) and isText:
                 links = getLinks(url)
                 for link in links:
-                    if not link in checked:
+                    if not link in checked.keys():
                         links2check[link] = url
         else:
             if status != 0:
-                logging.error("[%s is responding (status %d)]",url, status)
+                logging.error("[%s is responding (status %s)]",url, str(status))
+    for key, (s,o) in checked.iteritems():
+        logging.info("origin:%s --> %s (status:%s)", o, key, str(s))
 
 def usage():
     print 'check.py -s <starturl>'
