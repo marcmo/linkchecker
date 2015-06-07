9f8ae3235803c326248f6d943fcfe021114310c3
Oliver Mueller <oliver.mueller@esrlabs.com>
2015-06-07 20:10:05 +0200

    corrected rakefile

--- haskell/rakefile.rb
+++ haskell/rakefile.rb
@@ -8,7 +8,7 @@ end
 
 desc "profile haskell version"
 task :profile_haskell do
-  sh 'time cabal run profiling-linkcheck --ghc-options="RTS -p -s -h -i0.1"'
+  sh 'time cabal run profiling-linkcheck --ghc-options="+RTS -p -s -h -i0.1"'
   sh "hp2ps -e8in -c profiling-linkcheck.hp"
 end
 
